4ab98680f0a3e4c34b291ec72b46308fe422d774
fumingxu <1173494064@qq.com>
2020-10-26 12:13:15 +0800

    单例模式

--- springboot6/springboot-async/src/main/java/com/yingxue/lesson/task/jihe.java
+++ springboot6/springboot-async/src/main/java/com/yingxue/lesson/task/jihe.java
@@ -1,9 +1,8 @@
 package com.yingxue.lesson.task;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 
 public class jihe {
     public static void main(String[] args) {
@@ -13,19 +12,39 @@ public class jihe {
         map.put("ccc","333");
         map.put("ddd","444");
         map.put("eee","555");
-        for (String map2:map.keySet()){
-            System.out.println(map2);
+
+        for (String mmm : map.keySet()){
+            System.out.println(mmm+"------"+map.get(mmm));
         }
 
-        for (Map.Entry<String, String> map3:map.entrySet()){
-            System.out.println(map3.getKey()+"*****"+map3.getValue());
+        for (Map.Entry<String, String> nnnn:map.entrySet()){
+            System.out.println(nnnn+nnnn.getValue()+nnnn.getKey());
         }
+
+        Iterator<Map.Entry<String,String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<String,String> it = iterator.next();
+            System.out.println(it.getKey()+it.getValue());
+        }
+
+
+
 
 
         List list = new ArrayList();
-        list.add("aaa");
-        list.add(1);
-        String[] str = new String[2];
+        list.add("bbb");
+        list.add("ccc");
+        list.add("aaaa");
+        list.add(2,"ddd");
+        System.out.println("***************************************");
+        for (Object ll :list){
+            System.out.println(ll);
+        }
+        System.out.println("***************************************");
+        Iterator iterator1 = list.iterator();
+        while (iterator1.hasNext()){
+            System.out.println(iterator1.next());
+        }
 
         Thread t1 = new Thread(new Runnable() {
             @Override
@@ -33,5 +52,18 @@ public class jihe {
 
             }
         });
+
+        t1.start();
+
+        Callable callable = new Callable() {
+            @Override
+            public Object call() throws Exception {
+                return null;
+            }
+        };
+
+        FutureTask task = new FutureTask(callable);
+        new Thread(task).start();
+
     }
 }
